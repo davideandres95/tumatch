018aa512c1e614b1f61d27327f67e6678d74c1aa
Sam <ahmed.fouad@tum.de>
2022-11-19 15:24:26 +0100

    insertion logic

--- main.py
+++ main.py
@@ -10,9 +10,19 @@ app = Flask(__name__, template_folder="templates")
 order_book = dict()
 
 
+def update_quantity(old, new):
+    return min(old, new)
+
+
 def update_db(requests):
     for idx in requests.index:
         # TODO improve hashing alg, concatenation can fail if cols switched
+        order_str = '"{}" requested "{}" of security "{}" with action "{}"'.format(
+            requests["user"][idx],
+            requests["request"][idx],
+            requests["security"][idx],
+            requests["side"][idx],
+        )
         hash_seed = "{}{}{}{}".format(
             requests["user"][idx],
             requests["request"][idx],
@@ -23,18 +33,21 @@ def update_db(requests):
         hex_dig = hash_object.hexdigest()
         print(hex_dig)
         if order_book.get(hex_dig) is None:
-            print("Inserting the new order.. %s" % hash_seed)
+            print("Inserting the new order: %s" % order_str)
             order_book[hex_dig] = [requests["quantity"][idx], requests["price"][idx]]
         else:
             print(
                 "There is a similar order from same user already registered %s"
-                % hash_seed
+                % order_str
             )
             print("Updating the value of the previous order..")
             # TODO fix the quantity
             # TODO fix the price
-            order_book[hex_dig] = requests["quantity"][idx]
-            print("Order Updated to: %d" % order_book[hex_dig])
+            quantity = update_quantity(
+                order_book[hex_dig][0], requests["quantity"][idx]
+            )
+            order_book[hex_dig] = [quantity, requests["price"][idx]]
+            print("Order Updated to: %d" % order_book[hex_dig][0])
 
 
 def read_requests():
