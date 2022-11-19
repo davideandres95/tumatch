652cbe25f817407a1774fe60f74c70d43b1dd352
Sam <ahmed.fouad@tum.de>
2022-11-19 14:54:35 +0100

    updates

--- main.py
+++ main.py
@@ -3,23 +3,34 @@ from flask import Flask, render_template, abort, url_for, json, jsonify
 import json
 import numpy as np
 import pandas as pd
+import hashlib
 
 app = Flask(__name__, template_folder="templates")
 
-# def update_db(requests, request):
-#     requests['User'][]
+
+def update_db(requests):
+    for idx in requests.index:
+        # TODO improve hashing alg, concatenation can fail if cols switched
+        hash_seed ="{}{}{}{}".format(requests["user"][idx],requests["request"][idx],requests["security"][idx],requests["side"][idx])
+        hash_object = hashlib.sha1(hash_seed.encode("utf-8"))
+        hex_dig = hash_object.hexdigest()
+        print(hex_dig)
+
+
+def read_requests():
+    with open("./data/client_requests.json", "r") as myfile:
+        data = myfile.read()
+    return data
+
 
 @app.route("/")
 def requests_loading():
-    with open("./data/client_requests.json", "r") as myfile:
-        data = myfile.read()
-
-    data_element = json.loads(data)
+    data_element = json.loads(read_requests())
     requests = pd.json_normalize(data_element["AddOrderRequest"])
+    print("Requested Orders:")
     print(requests)
-    
-    # update_db(requests)
 
-    return render_template(
-        "requests_loading.html", title="page", jsonfile=json.dumps(data)
-    )
+    print("Filtered Orders:")
+    update_db(requests)
+
+    return "</p>Requests are loaded..</p>"
