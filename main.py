4c39c0f24f0e69784826b0a682cddac855424cd8
Sam <ahmed.fouad@tum.de>
2022-11-19 20:36:57 +0100

    implement sell and buy hashing dict

--- main.py
+++ main.py
@@ -7,8 +7,9 @@ import hashlib
 
 app = Flask(__name__, template_folder="templates")
 
-orderbook_history = dict()
-orderbook = dict()
+orderbook_history = {"Sell" : {}, "Buy" : {}}
+
+# TODO generally move to object oriented
 
 # add-add is cumlative buy/sell
 def update_order_quantity(old, new):
@@ -31,18 +32,18 @@ def update_db(requests):
             requests["side"][idx],
             requests["price"][idx]
         )
-        print(hash_seed)
+
         hash_object = hashlib.sha1(hash_seed.encode("utf-8"))
         hex_dig = hash_object.hexdigest()
 
-        inserted_order = orderbook_history.get(hex_dig)
-        if inserted_order is None:
+        request_type = requests["side"][idx]
+        if orderbook_history[request_type].get(hex_dig) is None:
             print(
                 "Received Order Hash: "
                 + hex_dig
                 + ": [  UNIQUE  ] new order registration: %s" % order_str
             )
-            orderbook_history[hex_dig] = [
+            orderbook_history[request_type][hex_dig] = [
                 idx, [requests["quantity"][idx]]
             ]
         else:
@@ -52,17 +53,18 @@ def update_db(requests):
                 + ": [DUPLICATED] similar order found: %s" % order_str
             )
             # new order override prev price
-            orderbook_history[hex_dig][1].append(requests["quantity"][idx])
+            orderbook_history[request_type][hex_dig][1].append(requests["quantity"][idx])
 
-            requests.loc[orderbook_history[hex_dig][0], 'quantity'] = update_order_quantity(
-                orderbook_history[hex_dig][1][0], requests["quantity"][idx]
+            requests.loc[orderbook_history[request_type][hex_dig][0], 'quantity'] = update_order_quantity(
+                orderbook_history[request_type][hex_dig][1][0], requests["quantity"][idx]
             )
             print(
                 "The previous order's quantity is updated to= {}".format(
-                    orderbook_history[hex_dig][1][0]
+                    orderbook_history[request_type][hex_dig][1][0]
                 )
             )
             requests = requests.drop(labels=idx, axis=0)
+    print(orderbook_history)
     return requests
 
 
