74c3252dcdf125f448b3183000852c91be0d4a23
ddeandres <davideandres95@gmail.com>
2022-11-19 07:37:46 +0100

    fixed SQL reserved word "order"

--- flaskr/db.py
+++ flaskr/db.py
@@ -9,7 +9,6 @@ def init_db():
     with current_app.open_resource('schema.sql') as f:
         db.executescript(f.read().decode('utf8'))
 
-
 @click.command('init-db')
 def init_db_command():
     """Clear the existing data and create new tables."""
@@ -26,13 +25,11 @@ def get_db():
 
     return g.db
 
-
 def close_db(e=None):
     db = g.pop('db', None)
 
     if db is not None:
         db.close()
-
 
 def init_app(app):
     app.teardown_appcontext(close_db)
