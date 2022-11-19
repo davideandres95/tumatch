159eccc3fba10d23c0bb727ba9bc01d33d433f08
Sam <ahmed.fouad@tum.de>
2022-11-19 23:02:32 +0100

    implement delete

--- main.py
+++ main.py
@@ -24,6 +24,7 @@ def update_order_quantity(old, new):
 
 
 def update_db(requests):
+    del_requests = []
     for idx in requests.index:
         # TODO improve hashing alg, concatenation can fail if cols switched
         order_str = '"{}" requested "{}" of security "{}" with action "{}"'.format(
@@ -43,8 +44,10 @@ def update_db(requests):
 
         hash_object = hashlib.sha1(hash_seed.encode("utf-8"))
         hex_dig = hash_object.hexdigest()
-
+        
+        new_quantity = 0
         request_type = requests["side"][idx]
+        calculated_index = idx
         if orderbook_history[request_type].get(hex_dig) is None:
             print(
                 "Received Order Hash: "
@@ -54,6 +57,8 @@ def update_db(requests):
             orderbook_history[request_type][hex_dig] = [
                 idx, [requests["quantity"][idx]]
             ]
+            new_quantity = requests["quantity"][idx]
+            calculated_index = idx
         else:
             print(
                 "Received Order Hash: "
@@ -74,6 +79,31 @@ def update_db(requests):
                 )
             )
             requests = requests.drop(labels=idx, axis=0)
+            # FIXME the index is alredy calculated why again?? calculated_index
+            calculated_index =  orderbook_history[request_type].get(hex_dig)[0]
+            print("XXXX calculated_index= " + str(calculated_index))
+
+        if requests["request"][calculated_index] == "Del":
+            del_origin_hash_seed = "[{}]{}+{}+{}:{}".format(
+                requests["user"][idx],
+                "Add", # FIXME workaround TODO
+                requests["security"][idx],
+                requests["side"][idx],
+                requests["price"][idx]
+            )
+            del_origin_hex_dig = hashlib.sha1(del_origin_hash_seed.encode("utf-8")).hexdigest()
+            del_requests.append((orderbook_history[request_type][del_origin_hex_dig][0], new_quantity))
+
+    print(del_requests)
+    # # Remove from Data base the Delete requests if they exist
+    # # TODO store locations of delete to improve perf
+    for delete_request_info  in del_requests:
+        print("Found a delete of index: " + str(delete_request_info[0]))
+        remaining = requests["quantity"][delete_request_info[0]] - delete_request_info[1]
+        if remaining > 0:
+            requests.loc[delete_request_info[0], 'quantity'] = remaining
+        else:
+            requests = requests.drop(labels=delete_request_info[0], axis=0)
 
     return orderbook_history, requests
 
@@ -85,6 +115,7 @@ def match(orderbook_history, requests):
         buy_price = requests['price'][buy_index_in_requests]
         buy_quantity = requests['quantity'][buy_index_in_requests]
         remaining = buy_quantity  # TODO obsolete
+        print("to buy: " + str(abs(remaining)))
         # scan all sell orders
         for sell_request_hash in orderbook_history['Sell']:
             sell_index_in_requests = orderbook_history['Sell'][sell_request_hash][0]
@@ -96,7 +127,7 @@ def match(orderbook_history, requests):
             sell_quantity = requests['quantity'][sell_index_in_requests]
 
             remaining = sell_quantity - remaining
-            print("to buy: " + str(remaining))
+            print("to buy remaining: " + str(abs(remaining)))
             print("to sell: " + str(sell_quantity))
             print("buying quantity: " + str(buy_quantity))
             print("selling quantity: " + str(sell_quantity))
