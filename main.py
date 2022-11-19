75750ae85ce6ae024ab47e9edfbd7c78514f2b78
Sam <ahmed.fouad@tum.de>
2022-11-19 15:18:21 +0100

    insertion logic

--- main.py
+++ main.py
@@ -7,14 +7,34 @@ import hashlib
 
 app = Flask(__name__, template_folder="templates")
 
+order_book = dict()
+
 
 def update_db(requests):
     for idx in requests.index:
         # TODO improve hashing alg, concatenation can fail if cols switched
-        hash_seed ="{}{}{}{}".format(requests["user"][idx],requests["request"][idx],requests["security"][idx],requests["side"][idx])
+        hash_seed = "{}{}{}{}".format(
+            requests["user"][idx],
+            requests["request"][idx],
+            requests["security"][idx],
+            requests["side"][idx],
+        )
         hash_object = hashlib.sha1(hash_seed.encode("utf-8"))
         hex_dig = hash_object.hexdigest()
         print(hex_dig)
+        if order_book.get(hex_dig) is None:
+            print("Inserting the new order.. %s" % hash_seed)
+            order_book[hex_dig] = [requests["quantity"][idx], requests["price"][idx]]
+        else:
+            print(
+                "There is a similar order from same user already registered %s"
+                % hash_seed
+            )
+            print("Updating the value of the previous order..")
+            # TODO fix the quantity
+            # TODO fix the price
+            order_book[hex_dig] = requests["quantity"][idx]
+            print("Order Updated to: %d" % order_book[hex_dig])
 
 
 def read_requests():
