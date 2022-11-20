0e869468699b23c864ebbe32fb915cac577dab3d
unknown <cristian.sandu@tum.de>
2022-11-20 05:09:21 +0200

    changes

---
+++ front/src/BOperations.js
@@ -0,0 +1,78 @@
+import * as React from 'react';
+import TextField from '@mui/material/TextField';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import Grid from '@mui/material/Grid';
+import DialogActions from '@mui/material/DialogActions';
+import Button from '@mui/material/Button';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
+import axios from 'axios';
+import Alert from '@mui/material/Alert';
+import {getContext} from './Context';
+import SvelteJSONEditor from './SvelteJSONEditor'
+
+
+
+export default function FormDialog(props) {
+  const [op, setOp] = React.useState('ADD');
+  const [errorMsg, setErrorMsg] = React.useState("");
+  const {isAuth, setAuth, token, setToken} = React.useContext(getContext());
+
+  const performeOperation = async (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    axios.post("", {
+      'request': 'ADD',
+      'side': op,
+      'quantity': data.get('quantity'),
+      'price': data.get('price'),
+      'security': data.get('securities'),
+      'user_token': token
+    }).catch((error) => {setErrorMsg(error.message)}).then((response) => {
+      if(response != undefined) {
+        this.close()
+      }    
+    }); 
+  }
+  const handleChange = (event) => {
+    setOp(event.target.value);
+  };
+
+  const [showEditor, setShowEditor] = React.useState(true);
+  const [readOnly, setReadOnly] = React.useState(false);
+  const [content, setContent] = React.useState({
+    json:
+        {
+            "user": "Omar",
+            "request": "Add",
+            "security": "AAPL",
+            "quantity": "1000",
+            "price": "2",
+            "side": "Sell"
+        },
+    text: undefined
+  });
+
+  return (
+    <form onSubmit={performeOperation}>
+      <DialogTitle>Add batch operation</DialogTitle>
+      <DialogContent>
+        <DialogContentText>
+          Write the operations as a JSON.
+        </DialogContentText>
+        <div className="my-editor">
+            <SvelteJSONEditor
+              content={content}
+              readOnly={readOnly}
+              onChange={setContent}
+            />
+          </div>
+        <DialogActions>
+          <Button variant="contained" type="submit">Perform</Button>
+        </DialogActions>
+      </DialogContent>
+    </form>
+  );
+}
--- front/src/Operation.js
+++ front/src/Operation.js
@@ -11,20 +11,45 @@ import MenuItem from '@mui/material/MenuItem';
 import axios from 'axios';
 import Alert from '@mui/material/Alert';
 import {getContext} from './Context';
+import Checkbox from '@mui/material/Checkbox';
 
 export default function FormDialog(props) {
-  const [op, setOp] = React.useState('ADD');
+  const [op, setOp] = React.useState('');
+  const [cb, setCb] = React.useState(1);
+
   const [errorMsg, setErrorMsg] = React.useState("");
   const {isAuth, setAuth, token, setToken} = React.useContext(getContext());
+
+  const validate = (data) => {
+    console.log(data);
+    if(op == '') {
+      setErrorMsg("You need to select an operation: BUY / SELL")
+      return false;
+    } else if (data.get('quantity') <= 0) {
+      setErrorMsg("Quantity amount must be positive")
+      return false;
+    } else if (data.get('price') <= 0 && cb == 1) {
+      setErrorMsg("Price must be positive")
+      return false;
+    } else if (data.get('securities').length <= 2) {
+      setErrorMsg("Security length must be longer than 2")
+      return false
+    }
+    return true;
+  }
 
   const performeOperation = async (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
+    if(validate(data) == false){
+      return;
+    }
+    const price = cb == 1 ? data.get('price') : "MARKET";
     axios.post("", {
       'request': 'ADD',
       'side': op,
       'quantity': data.get('quantity'),
-      'price': data.get('price'),
+      'price': price,
       'security': data.get('securities'),
       'user_token': token
     }).catch((error) => {setErrorMsg(error.message)}).then((response) => {
@@ -32,6 +57,7 @@ export default function FormDialog(props) {
         this.close()
       }    
     }); 
+    
   }
   const handleChange = (event) => {
     setOp(event.target.value);
@@ -44,7 +70,7 @@ export default function FormDialog(props) {
         <DialogContentText>
           The operations are: BUY, SELL
         </DialogContentText>
-        <Grid container spacing={3}>
+        <Grid container spacing={1}>
           <Grid item xs={12}>
               <TextField
               autoFocus
@@ -56,8 +82,14 @@ export default function FormDialog(props) {
               variant="standard"
             />
           </Grid>
-          <Grid item xs={12}>
+          <Grid item container xs={12}>
+            <Grid item xs={6} sx={{'margin-top': 20}}>
+              Default Market price: 
+              <Checkbox defaultChecked onChange={()=>{setCb(cb*-1)}} />
+            </Grid>
+            <Grid item xs={6}>
               <TextField
+              disabled={cb == 1}
               autoFocus
               margin="dense"
               id="price"
@@ -65,7 +97,8 @@ export default function FormDialog(props) {
               label="Price (in USD)"
               fullWidth
               variant="standard"
-            />
+              />
+            </Grid>
           </Grid>
           <Grid item xs={12}>
               <TextField
